9f15682a3db19d8a71fc86f057c4d33d4ffe5638
bang-nugraha <bangnugarha179@gmail.com>
2022-01-04 21:34:51 +0700

    percabangan tingkat lanjut

--- main.py
+++ main.py
@@ -13,10 +13,17 @@ print('Maka Budi berangkat ke toko dan mulai berbelanja.')
 
 # Percabangan
 jumlah_botol_susu = 173
+jumlah_telur = 1500
+
+print(f"Jumlah susu yang ada di toko: {jumlah_botol_susu} botol.")
+print(f"Jumlah telur yang ada di toko: {jumlah_telur} butir.")
 
 if jumlah_botol_susu > 0:
     print("Budi mengecek harganya dan ternyata uangnya cukup.")
-    print("Budi membeli 1 botol susu.")
+    if jumlah_botol_susu == 0:
+        print("Budi membeli 1 botol susu.")
+    else:
+        print("Budi membeli 6 botol susu.")
 else:
     print("Budi tidak jadi membeli satu 1 susu.")
 
