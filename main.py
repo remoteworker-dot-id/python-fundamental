5c2b8f7c449004ca31d07426273129ab1fba3023
bang-nugraha <bangnugarha179@gmail.com>
2022-01-04 21:16:52 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+Tiga sintaksis dasar dalam pemrograman adalah:
+1. Sekuensial
+2. Percabangan
+3. Perulangan
+"""
+
+# Sekuensial
+print('Ibu berkata, "Pergi belanja ke toko."')
+print('Budi menjawab, "Baik Bu, apa yang harus saya beli?"')
+print('Ibu menjawab, "Beli 1 botol susu dan jika ada telur, beli 6."')
+print('Maka Budi berangkat ke toko dan mulai berbelanja.')
+
+# Percabangan
+jumlah_botol_susu = 173
+
+if jumlah_botol_susu > 0:
+    print("Budi mengecek harganya dan ternyata uangnya cukup.")
+    print("Budi membeli 1 botol susu.")
+else:
+    print("Budi tidak jadi membeli satu 1 susu.")
+
+print("Budi pulang ke rumah.")
+print("Menyampaikan hasilnya kepada ibu.")
