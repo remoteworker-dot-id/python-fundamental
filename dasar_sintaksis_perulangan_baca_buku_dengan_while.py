f83451a72f8ab94928a434f2f47254de9f8b7b5b
bang-nugraha <bangnugarha179@gmail.com>
2022-01-09 20:19:32 +0700

    perulangan menggunakan while dengan jumlah tak terbatas dan ada variabel pemberhenti

--- dasar_sintaksis_perulangan_baca_buku_dengan_while.py
+++ dasar_sintaksis_perulangan_baca_buku_dengan_while.py
@@ -2,13 +2,24 @@
 Program perulangan membaca buku dengan while
 """
 jumlah_buku = 10
+total_jumlah_baca = 0
 print('Ibu berkata, "Bacalah semua bukumu!"')
 
-jumlah_buku_yang_sudah_dibaca = 0
-print(f'Jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
+jumlah_buku_yang_sudah_dibaca_dan_dipahami = 0
+print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
 
-while jumlah_buku_yang_sudah_dibaca < jumlah_buku:
-    jumlah_buku_yang_sudah_dibaca += 1
-    print(f"Buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca.")
+while total_jumlah_baca < jumlah_buku * 2:
+    total_jumlah_baca += 1
+    if jumlah_buku_yang_sudah_dibaca_dan_dipahami == 9:
+        print(f"Buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1} belum paham.")
+    else:
+        jumlah_buku_yang_sudah_dibaca_dan_dipahami += 1
+        print(f"Buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} sudah dibaca dan dipahami.")
 
-print(f"Jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}")
+print(f"Jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}")
+
+if jumlah_buku_yang_sudah_dibaca_dan_dipahami == jumlah_buku:
+    print('"Bu, semua buku sudah dibaca dan dipahami."')
+else:
+    print(f'"Bu, tidak semua buku bisa dipahami. '
+          f'Budi hanya bisa memahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami} buku."')
